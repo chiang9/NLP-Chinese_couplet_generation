63cf2ba15bfb0b5b3bf82e5b3b596833b0991a9d
Zalelin3 <shihaol@students.pitzer.edu>
2021-12-01 02:15:27 +0800

    main update

--- utils/generate_couplet.py
+++ utils/generate_couplet.py
@@ -6,6 +6,8 @@ import torch
 import sys
 sys.path.append('.../model')
 from model.fusionDataset import FusionDataset
+import torch.nn.functional as F
+
 
 def greedy_decode(model,bert,tokenizer,
                   sent,glyph2ix,
@@ -16,6 +18,7 @@ def greedy_decode(model,bert,tokenizer,
     Output dimension is glyph dim.
     return in list format e.g.(['1','2','3'])
     """
+    
     bert.to(device)
     model.to(device)
     
@@ -84,13 +87,116 @@ def greedy_decode(model,bert,tokenizer,
 
         # FYI, while you implement the beam search
         # add F.log_softmax(out, dim=-1) to it to get acutual log prob
-        _,next_word = torch.max(prob,dim=0)
+        _,next_word = torch.max(prob,dim=-1)
         # print(next_word)
         next_word = ix2glyph[next_word.item()]
         
         ys.append(next_word)
     return ys
 
+def beam_search_decode(model,k,bert,tokenizer,
+                      sent,glyph2ix,
+                      pinyin2ix,pos2ix,
+                      ix2glyph,device):
+    def get_last_log_prob(sent,memory,model=model,\
+                     tokenizer=tokenizer,\
+                     glyph2ix=glyph2ix,\
+                     pinyin2ix=pinyin2ix,\
+                     pos2ix=pos2ix,bert=bert,\
+                     encode=False,\
+                     skip_error=False,\
+                     device=device):
+        """
+        helper function to get the last word log prob
+        return torch([n]), where n is dimension of output
+        """
+        model.eval()
+        
+        Ysents_input_ids,Ysents_token_type_ids,\
+        Ysents_attention_mask,Ysents_pinyin_ids,\
+        Ysents_glyph_ids,Ysents_pos_ids,\
+        trueY,y_mask_ids\
+        = FusionDataset.prepare_sequence(sents=[sent],\
+                                         tokenizer=tokenizer,\
+                                         glyph2ix=glyph2ix,\
+                                         pinyin2ix=pinyin2ix,\
+                                         pos2ix=pos2ix,\
+                                         encode=False,\
+                                         skip_error=False,\
+                                         device=device)
+
+        Yword_embeddings = bert(Ysents_input_ids,\
+                                Ysents_token_type_ids,\
+                                Ysents_attention_mask \
+                               )['last_hidden_state'].detach()
+
+        decode_input = {
+                        'memory':memory,\
+                        'Xpad_hidden_mask':None,\
+                        'Yword_embeddings':Yword_embeddings,\
+                        'Ysents_pinyin_ids':Ysents_pinyin_ids, \
+                        'Ysents_glyph_ids':Ysents_glyph_ids,\
+                        'Ysents_pos_ids':Ysents_pos_ids,\
+                        'Ypad_hidden_mask':None,\
+                        'tgt_mask':y_mask_ids}
+
+        out = model.decode(**decode_input)
+        out = model.Linear(out[-1,0,:])
+        # get the latest generate word
+        prob = F.log_softmax(out,dim=-1)
+        return prob
+    
+    bert.to(device)
+    model.to(device)
+    
+    model.eval()
+    # Generate encoder input
+    Xsents_input_ids,Xsents_token_type_ids, \
+    Xsents_attention_mask,Xsents_pinyin_ids,\
+    Xsents_glyph_ids,\
+    Xsents_pos_ids = FusionDataset.prepare_sequence(sents=[sent],
+                                                    tokenizer=tokenizer,
+                                                    glyph2ix=glyph2ix,
+                                                    pinyin2ix=pinyin2ix,
+                                                    pos2ix=pos2ix,
+                                                    encode=True,
+                                                    skip_error=False,
+                                                    device=device)
+
+    Xword_embeddings = bert(Xsents_input_ids, \
+                         Xsents_token_type_ids, \
+                         Xsents_attention_mask  \
+                         )['last_hidden_state'].detach()
+    encode_input = {'Xword_embeddings':Xword_embeddings,
+                    'Xsents_pinyin_ids':Xsents_pinyin_ids, \
+                    'Xsents_glyph_ids':Xsents_glyph_ids,\
+                    'Xsents_pos_ids':Xsents_pos_ids,
+                    'Xpad_hidden_mask':None}
+    # ENCODER 
+    memory = model.encode(**encode_input)
+    
+    sequences = [[list(),0.0]]
+    for _ in range(len(sent)):
+        all_candidates =list()
+        
+        # expand each current candidate
+        for i in range(len(sequences)):
+            seq, score = sequences[i]
+            
+            last_word_prob = get_last_log_prob(seq,memory)
+            
+            values, indices = last_word_prob.topk(k,largest=False)
+            for j in range(k):
+                w = ix2glyph[indices[j].item()]
+                candidate = [seq+[w], score - values[j]]
+                all_candidates.append(candidate)
+                
+        # order all candidates by score
+        ordered = sorted(all_candidates, key=lambda t:t[1])
+        
+        # select k best
+        sequences = ordered[:k]
+    return sequences
 
 def greedy_decode2(model,bert,tokenizer,
                   sent,glyph2ix,
